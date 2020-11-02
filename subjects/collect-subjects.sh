e17c3a77b727e1523a6775c6e2fe3b8354e6b160
pderakhshanfar <pouria.derakhshanfar@gmail.com>
2020-11-02 14:18:53 +0100

    Select bugs

---
+++ subjects/collect-subjects.sh
@@ -0,0 +1,28 @@
+# This script collects all of the subjects for experiment using Defects4j v2.0
+
+
+IFS=$'\n' read -rd '' -a pids <<<"$(defects4j pids)"
+
+
+echo " Number of projects in Defects4j: ${#pids[@]}"
+echo "Iterating on projects ..."
+
+mkdir subjects/temp
+echo "project_id,bug_id,date_fixed" > subjects/bugs.csv
+for project_id in "${pids[@]}"
+do
+    # echo "$(defects4j bids -p $project_id)"
+    IFS=$'\n' read -rd '' -a bids <<<"$(defects4j bids -p $project_id)"
+    echo "Project id: $project_id - number of active bugs: ${#bids[@]}"
+
+    defects4j query -p $project_id -q "revision.date.fixed" > "subjects/temp/$project_id.csv"
+
+    python subjects/python/sort.py "subjects/temp/$project_id.csv"
+
+    echo "10 most recently fixed bugs are selected"
+done
+
+
+rm -rf subjects/temp
+
+
---
+++ subjects/python/sort.py
@@ -0,0 +1,31 @@
+import csv
+import sys
+import os
+import operator
+
+file1 = open(sys.argv[1],'r')
+
+csv1 = csv.reader(file1,delimiter=",")
+
+sort = sorted(csv1,key=operator.itemgetter(1),reverse = True)
+
+# for eachline in sort:
+#     print eachline
+
+
+project_id = os.path.splitext(os.path.basename(sys.argv[1]))[0]
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+finalList = os.path.join(dir_path,"..","bugs.csv")
+
+outputFile = open(finalList,"a")
+outputFileWriter = csv.writer(outputFile)
+
+
+index=0
+while index < len(sort) and index < 10:
+    outputFileWriter.writerow([project_id,sort[index][0],sort[index][1]])
+    index+=1
+
+
+    
