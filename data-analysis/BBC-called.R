9760f42822c7156cd35b59650438ce94899d1176
Pouria Derakhshanfar <pouria.derakhshanfar@gmail.com>
2021-09-07 12:48:40 +0300

    Update motiv plots and analysis script

--- data-analysis/BBC-called.R
+++ data-analysis/BBC-called.R
@@ -16,7 +16,59 @@ raw_df <- raw_df %>%
          usefulness_rate_eval = useful/as.numeric(ff_eval),
          usefulness_rate_activated = useful/activated,
          )
+### TEMP
 
+temp <- raw_df %>%
+  group_by(project_id, bug_id, target_class, execution_id) %>%
+  summarise(activated_objectives = sum(is_activated),
+            used_for_objectives = sum(is_useful),
+            total_objectives = n()) %>%
+  mutate(activation_perc = activated_objectives/total_objectives,
+         usefulness_perc = used_for_objectives/total_objectives)
+
+
+
+objectives_df <- temp %>%
+  group_by(project_id) %>% 
+  summarise(
+    # activation rate
+    activation_mean = mean(activation_perc),
+    activation_median = median(activation_perc),
+    activation_sd = sd(activation_perc),
+    activation_min = min(activation_perc),
+    activation_max = max(activation_perc),
+    # usefulness rate
+    usefulness_mean = mean(usefulness_perc),
+    usefulness_median = median(usefulness_perc),
+    usefulness_sd = sd(usefulness_perc),
+    usefulness_min = min(usefulness_perc),
+    usefulness_max = max(usefulness_perc)
+  )
+
+p <- temp %>%
+  ggplot(aes(x = project_id, y = usefulness_perc)) +
+  geom_boxplot() +
+  stat_summary(
+    fun = mean,
+    geom = "point",
+    shape = 0,
+    color = "black",
+    fill = "white"
+  )
+ggsave("output/motiv/motiv_BBC_useful_perc.pdf", width = 16, height = 4)
+  
+
+p <- temp %>%
+  ggplot(aes(x = project_id, y = activation_perc)) +
+  geom_boxplot() +
+  stat_summary(
+    fun = mean,
+    geom = "point",
+    shape = 0,
+    color = "black",
+    fill = "white"
+  )
+ggsave("output/motiv/motiv_BBC_actiavtion_perc.pdf", width = 16, height = 4)
 
 df_total <- raw_df %>%
   filter(is_activated == 1)%>%
@@ -25,6 +77,7 @@ df_total <- raw_df %>%
             usecall = mean(usefulness_rate_called),
             activation = mean(activation_rate_eval),
             useact = mean(usefulness_rate_activated))
+
 
 
 # df_total_temp <- df_total %>%
@@ -90,3 +143,44 @@ p <- df_total %>%
   geom_boxplot() +
   scale_y_log10()
 ggsave("output/motiv/motiv_BBC_activated_log10_scaled.pdf", width = 16, height = 4)
+
+
+### Cor
+
+# for bbc 
+
+cor_df <- df_total %>%
+  group_by(project_id,bug_id,target_class) %>%
+  summarise(activation_mean = mean(activation),
+                usefulness_mean = mean(useeval))
+results_df <- getResults() %>%
+  filter(
+    configuration =="DynaMOSA" |
+    configuration == "bbc-0.5"
+  ) %>%
+  select(project,bug_id,configuration,execution_idx,TARGET_CLASS,BranchCoverage)
+
+branchCoverag_diff <- results_df %>% 
+  filter(configuration =="DynaMOSA") %>% 
+  inner_join(
+    results_df %>% 
+      filter(configuration =="bbc-0.5"),
+    by = c('project', 'bug_id', 'TARGET_CLASS'),
+    suffix = c('.dynamosa', '.bbc')
+  ) %>% 
+  filter(execution_idx.bbc == execution_idx.dynamosa) %>%
+  mutate(diff = BranchCoverage.bbc - BranchCoverage.dynamosa) %>%
+  group_by(project,bug_id,TARGET_CLASS) %>%
+  summarise(avg_diff = mean(diff)) %>%
+  rename(target_class = TARGET_CLASS ,
+         project_id = project)
+
+
+final_df <- cor_df %>%
+  inner_join(
+    branchCoverag_diff,
+    by=c('project_id','bug_id','target_class')
+  )
+
+
+cor.test(final_df$usefulness_mean, final_df$avg_diff, method="spearman")
