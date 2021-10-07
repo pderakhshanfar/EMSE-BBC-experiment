671f470707e93b74230bb297cbad15e04d70e142
Pouria Derakhshanfar <pouria.derakhshanfar@gmail.com>
2021-10-07 19:25:03 +0200

    Add dockerignore

--- scripts/docker/run-test-generation-container.sh
+++ scripts/docker/run-test-generation-container.sh
@@ -1,9 +1,17 @@
-# After building the the image, we run the container
-
 COMMAND=$1
 
 ${COMMAND} stop test-generation-container
 ${COMMAND} rm test-generation-container
 
-${COMMAND} run -dit -u ${UID} -v ${PWD}:/experiment --name test-generation-container  \
+${COMMAND} run -dit -u ${UID} --name test-generation-container  \
+--mount type=bind,source="$(pwd)/subjects",target=/experiment/subjects \
+--mount type=bind,source="$(pwd)/defects4j",target=/experiment/defects4j \
+--mount type=bind,source="$(pwd)/results",target=/experiment/results \
+--mount type=bind,source="$(pwd)/scripts",target=/experiment/scripts \
+--mount type=bind,source="$(pwd)/results/SEED",target=/experiment/SEED \
+--mount type=bind,source="$(pwd)/tools",target=/experiment/tools \
+--mount type=bind,source="$(pwd)/configurations",target=/experiment/configurations \
+--mount type=bind,source="$(pwd)/console-logs",target=/experiment/console-logs \
+--mount type=bind,source="$(pwd)/libs",target=/experiment/libs \
+--mount type=bind,source="$(pwd)/data",target=/experiment/data \
 test-generation-img
